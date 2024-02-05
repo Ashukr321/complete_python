04c2014550d34619e7b951ff6c7c141d67080964
Ashukr321 <coderashukr321@gmail.com>
2024-02-05 14:44:15 +0530

    complete email verification

--- python_projects/qr_code_generator/qr5.py
+++ python_projects/qr_code_generator/qr5.py
@@ -2,14 +2,15 @@ import qrcode as qr
 
 data = qr.QRCode(
   version=1,error_correction=qr.ERROR_CORRECT_H,
-  border=1,
+  border=4,
   box_size= 12
 );
 
 # data 
+
 data.add_data("https://youtu.be/l9oeYIJVycA?si=1d-ZQ048BKzEGZDU");
 data.make(fit=True);
-img  = data.make_image(fill_color= "red",black_color="white");
+img  = data.make_image(fill_color= "blue",black_color="white");
 img.save("red.png");
 
 img.show();
